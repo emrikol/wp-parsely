fc9e1a1454c32a9c870419c069ee85b2cf00bc64
Josh Romero <josh@joshromero.com>
2018-03-19 11:58:57 -0500

    Eliminate XSS vector. Create a new DOM node programmatically, then attach it to the DOM.

--- parsely-admin-header.php
+++ parsely-admin-header.php
@@ -9,17 +9,22 @@
 (function($) {
     $(document).ready(function onDOMReady() {
         var apikey = $('#apikey').val();
-        var recrawlRequiredMessage = '<strong style="color: red">Important:' +
-            '</strong> changing this value on a site currently tracked with ' +
+        var recrawlRequiredHTML = '<p class="description">' +
+            '<strong style="color: red">Important:</strong> ' +
+            'changing this value on a site currently tracked with ' +
             'Parse.ly will require reprocessing of your Parse.ly data. Once ' +
             'you have changed this value, please contact ' +
-            '<a href="mailto:support@parsely.com?subject=Please reprocess ' +
-            apikey + '">support@parsely.com</a> to kick off reprocessing of ' +
-            'your data.';
+            '<a href="mailto:support@parsely.com?subject=Please reprocess">' +
+            'support@parsely.com</a> to kick off reprocessing of ' +
+            'your data.' +
+            '</p>';
+        var recrawlRequiredContainer = $(recrawlRequiredHTML);
 
-        $('<p class="description"></p>')
-            .appendTo("div.parsely-form-controls[data-requires-recrawl='true'] .help-text")
-            .html(recrawlRequiredMessage);
+        var a = recrawlRequiredContainer.find('a');
+        a.attr( 'href', 'mailto:support@parsely.com?subject=Please reprocess ' + apikey);
+
+        $("div.parsely-form-controls[data-requires-recrawl='true'] .help-text")
+            .append(recrawlRequiredContainer)
     });
 })(jQuery);
 </script>
